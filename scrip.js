03383aa8491794f39d1ba856a62ea3f77bbcc8a4
ajijahimutkiyut-bit <cicijijey@gmail.com>
2025-09-30 14:21:23 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,6 @@
+const tombolsapa=document.gentElemenByld('sapaButton') 
+// 2. Tambahkan 'event listiner' untuk aksi 'click'
+tombolsapa.addEventlistener( 'click', functioon() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung!');
+  });
